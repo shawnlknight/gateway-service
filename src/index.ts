22672687068aae7e1dc0d73edb5f9d89f8d48693
shawn.leberknight <shawn.leberknight@gmail.com>
2021-10-21 14:07:06 -0400

    add formatting & linting configs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import * as express from "express";
-import { graphqlHTTP } from "express-graphql";
-import { stitchSchemas } from "@graphql-tools/stitch";
-import { loadSchema } from "@graphql-tools/load";
-import { UrlLoader } from "@graphql-tools/url-loader";
+import * as express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { stitchSchemas } from '@graphql-tools/stitch'
+import { loadSchema } from '@graphql-tools/load'
+import { UrlLoader } from '@graphql-tools/url-loader'
 
 async function makeGatewaySchema() {
   // Get subschema
-  const inventorySchema = await loadSchema("http://localhost:4001/graphql", {
+  const inventorySchema = await loadSchema('http://localhost:4001/graphql', {
     loaders: [new UrlLoader()],
-  });
+  })
 
   return stitchSchemas({
     subschemas: [
@@ -16,23 +16,23 @@ async function makeGatewaySchema() {
         schema: inventorySchema,
       },
     ],
-  });
+  })
 }
 
 const app = async () => {
-  const schema = await makeGatewaySchema();
-  const server = express();
+  const schema = await makeGatewaySchema()
+  const server = express()
   server.use(
-    "/graphql",
-    graphqlHTTP((req) => ({
+    '/graphql',
+    graphqlHTTP((request) => ({
       schema,
-      context: { authHeader: req.headers.authorization },
+      context: { authHeader: request.headers.authorization },
       graphiql: true,
-    }))
-  );
-  server.listen(4000, () =>
-    console.log("gateway running at http://localhost:4000/graphql")
-  );
-};
+    })),
+  )
+  server.listen(4000, () => {
+    console.log('gateway running at http://localhost:4000/graphql')
+  })
+}
 
-export default app();
+export default app()
